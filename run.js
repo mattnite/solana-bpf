df1682715b135d32dd3f2a9e300ac58f7809a66f
Matt Knight <mattnite@protonmail.com>
2021-09-06 13:33:11 -0700

    shared object baybeeee

--- run.js
+++ run.js
@@ -1,42 +1,49 @@
 const clangFactory = require('./build/clang-solana-bpf.js');
+const lldFactory = require('./build/lld.js');
 
 (async () => {
   const clang = await clangFactory({ noInitialRun: true });
+  const lld = await lldFactory({ noInitialRun: true });
 
   clang.FS.mkdir('/work');
   clang.FS.mount(clang.NODEFS, { root: '.' }, '/work');
+  lld.FS.mkdir('/clang');
+  lld.FS.mount(lld.PROXYFS, {
+    root: "/",
+    fs: clang.FS
+  }, "/clang");
 
-  const ret = clang.callMain([
-    // "--linker-option=-z notext",
-    // "--linker-option=--shared",
-    // "--linker-option=--Bdynamic /usr/share/bpf.ld",
-    // "--linker-option=--entry=entrypoint",
-
+  let ret = clang.callMain([
     "-Werror",
     "-O2",
     "-fno-builtin",
     "-std=c17",
-
-    "-std=c++17",
-    // "-fPIC",
-    // "-fomit-frame-pointer",
-    // "-fno-exceptions",
-    // "-fno-asynchronous-unwind-tables",
-    // "-fno-unwind-tables",
-
-    "-I/usr/include/clang",
-    "-I/usr/include/solana",
-
+    "-isystem/usr/include/clang",
+    "-isystem/usr/include/solana",
+    "-mrelocation-model", "pic",
+    "-pic-level", "2",
     "-triple", "bpfel-unknown-unknown-bpfel+solana",
     "-emit-obj",
 
-    "-o", "/work/sha.so",
-    "-version",
-
-    "-x", "c++",
+    "-o", "/work/sha.o",
     "/work/test/sha.c",
   ]);
   if (ret != 0) {
     console.log('compiler exited with ' + ret);
+  }
+
+  ret = lld.callMain([
+    "-flavor", "ld",
+    "-z", "notext",
+    "-shared",
+    "--Bdynamic",
+    "/clang/usr/share/bpf.ld",
+    "--entry", "entrypoint",
+    "-o", "/clang/work/sha.so",
+    "/clang/work/sha.o",
+    "/clang/usr/lib/libcompiler_builtins.rlib",
+  ]);
+  if (ret != 0) {
+    console.log('lld exited with ' + ret);
   }
 })();
